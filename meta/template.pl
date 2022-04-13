8b72faa4a43da8f4dbb206e7c6798e53799d219e
David Hoekman <sqrtneginf@gmail.com>
2022-04-12 17:23:21 -0700

    initial commit of task meta/template.pl

---
+++ meta/template.pl
@@ -0,0 +1,37 @@
+#!/usr/local/bin/perl
+#u# http://rosettacode.org/wiki/xx
+#t# inprogress
+#c# 2022-04-xx >RC
+#p# OK
+
+use Data::Printer;          # debug only
+use Time::HiRes qw(sleep);  # debug only
+
+my @res;
+
+use strict;
+use warnings;
+use feature 'say';
+
+# code goes here
+
+say my $result = join "\n", @res;
+my $ref = <<'END';
+END
+
+use Test::More;
+chomp $ref;
+is($result, $ref);
+done_testing();
+
+__END__
+
+=={{header|Perl}}==
+{{trans|xx}}
+{{libheader|ntheory}}
+<lang perl>
+</lang>
+{{out}}
+<pre style="height:20ex">
+</pre>
+[https://github.com/SqrtNegInf/Rosettacode-Perl5-Smoke/blob/master/ref/xx xx]
